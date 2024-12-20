4248f6d36333abd8ac57a171043fd6094c9d31c2
Rajuk75 <rajukumar957259@gmail.com>
2024-12-20 14:02:30 +0530

    Add index.html, main.js, styles.css

---
+++ main.js
@@ -0,0 +1,24 @@
+// onclick="alert('Hello Noida')"
+// console.log("Hello i am from Bihar Muzaffarpur");
+
+
+// var myName = "Raju Kumar";
+
+// a=5,b=10;
+// c=a+b;
+// console.log(c);
+
+// console.log(myName);
+
+// console.log(parseInt("   123   "));
+
+
+const sum=(a,b)=>{
+    return a+b;
+}
+const sum=(a,b,c)=>{
+    return a+b+c;
+}
+console.log(sum(5,3));
+console.log(sum(5,3,7));
+
